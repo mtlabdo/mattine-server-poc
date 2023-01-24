bfa4eae65d8cf1f4538eb14e050f1582b652c56d
mtlabdo <abdessamadmaatallaoui@gmail.com>
2023-01-24 11:25:43 +0100

    remove dist

--- dist/dtos/user/user.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserDto = void 0;
-class UserDto {
-    constructor() { }
-}
-exports.UserDto = UserDto;
-//# sourceMappingURL=user.dto.js.map
